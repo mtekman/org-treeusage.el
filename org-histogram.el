f47a9025a0f1ae24cf5417fa61a023a0de6a21c0
Mehmet Tekman <mtekman89@gmail.com>
2020-04-03 03:27:52 +0200

    added function defs, working with overlays

--- org-histogram.el
+++ org-histogram.el
@@ -5,18 +5,35 @@
 (defvar prev-key nil)
 
 
-(defun make-key (level header) (cons level header))
-(defun make-values (line perc) (list :nlines line :percent perc))
-(defun get-lineinf (info keyf) (line-number-at-pos (plist-get info keyf)))
-(defun calc-percentage (child par) (/ (float (* 100 child)) par))
-(defun calc-nlines (info) (- (get-lineinf info :end)
-                             (get-lineinf info :begin)))
-(defun next-match (point1) (progn (org-next-visible-heading 1)
-                                  (not (eq point1 (point)))))
+(defun make-key (level header)
+  "Generate key for hash using LEVEL and HEADER."
+  (cons level header))
+
+(defun make-values (line perc bounds)
+  "Create a value from LINE and PERC BOUNDS set"
+  (list :nlines line :percent perc :bounds bounds))
+
+(defun get-lineinf (info keyf)
+  "Get line number for specific INFO keyword KEYF."
+  (line-number-at-pos (plist-get info keyf)))
+
+(defun calc-percentage (child par)
+  "Calculate percentage of CHILD against PAR.
+Percentages are not super accurate, but are a good gauge."
+  (/ (float (* 100 child)) par))
+
+(defun calc-nlines (info)
+  "Calculate the number of lines from INFO."
+  (- (get-lineinf info :end)
+     (get-lineinf info :begin)))
+
+(defun next-match (point1)
+  "Get next match from current POINT1."
+  (progn (org-next-visible-heading 1)
+         (not (eq point1 (point)))))
 
 (defun setroot ()
-  "Perform once.
-Jump to end and beginning to get full bounds."
+  "Perform once. Get full bounds."
   (let ((dline (- (progn (end-of-buffer)
                          (org-backward-sentence)
                          (line-number-at-pos (point)))
@@ -24,11 +41,11 @@ Jump to end and beginning to get full bounds."
                          (org-next-visible-heading 1)
                          (line-number-at-pos (point)))))
         (dkey (make-key 0 nil)))
-    ;;
-    (puthash dkey (make-values dline nil) hasher)
+    (puthash dkey (make-values dline nil nil) hasher)
     (push dkey prnt-alist)))
 
-(defun get-parent (lvl-now)
+(defun update-parent (lvl-now)
+  "Get or update parent from current level LVL-NOW."
   (let ((prev-lvl (car prev-key))
         (prev-hdr (cdr prev-key))
         (curr-parent (car prnt-alist)))
@@ -46,6 +63,21 @@ Jump to end and beginning to get full bounds."
            (car prnt-alist))
           (t curr-parent))))
 
+(defun get-title-bounds (info)
+  "Get title and bounds from INFO."
+  (let ((head (plist-get info :title))
+        (bend (plist-get info :contents-begin))
+        (bbeg (line-beginning-position)))
+    (when head
+      (save-excursion
+        (goto-char bbeg) ;; important
+        (let* ((end (search-forward-regexp
+                     ;; Capture: [1/1] User
+                     (shell-quote-argument head) bend))
+               (beg (match-beginning 0)))
+          `(,head . (,beg . ,end)))))))
+
+
 (defun process-visible ()
   "The idea is to get stats only for the visible portions of the buffer.
 To investigate further, expand a heading."
@@ -57,61 +89,91 @@ To investigate further, expand a heading."
       (setroot)
       ;; Jump to beginning and parse headers
       (goto-char 0)
-      ;;
       (while (next-match (point))
-        (let* ((info (cadr (org-element-at-point)))
-               (head (plist-get info :title))
-               (level (plist-get info :level)))
-          (when head
-            ;; Check and update parent
-            (setq prnt-curr (get-parent level))
-            ;;
-            (let ((dline (calc-nlines info))
-                  (elkey (make-key level head))
-                  (_prnt-inf (gethash prnt-curr hasher)))
-              (let ((percent (calc-percentage
-                              dline
-                              (plist-get _prnt-inf :nlines))))
-                (puthash elkey (make-values dline percent) hasher)
-                (setq prev-key elkey)))))))
+        (let ((info (cadr (org-element-at-point))))
+          (let ((level (plist-get info :level))
+                (_bound (get-title-bounds info)))
+            (let ((head (car _bound))
+                  (hrng (cdr _bound)))
+              (when head
+                ;; Check and update parent
+                (setq prnt-curr (update-parent level))
+                ;;
+                (let ((dline (calc-nlines info))
+                      (elkey (make-key level head))
+                      (_prnt-inf (gethash prnt-curr hasher)))
+                  (let ((percent (calc-percentage
+                                  dline
+                                  (plist-get _prnt-inf :nlines))))
+                    (puthash elkey
+                             (make-values dline percent hrng)
+                             hasher)
+                    (setq prev-key elkey)))))))))
     hasher))
 
 
-(setq tmp (with-current-buffer "lorum.org" (process-visible)))
+(defun set-overlays (hashtable)
+  "Set the overlays from HASHTABLE."
+  (maphash
+   (lambda (head info)
+     (let ((bounds (plist-get info :bounds))
+           (nlines (plist-get info :nlines))
+           (percent (plist-get info :percent)))
+       (if percent
+           (let ((ov (make-overlay (car bounds) (cdr bounds))))
+             (overlay-put ov :org-histogram t)
+             (overlay-put ov 'display (format "%2.1f%% {%d}"
+                                              percent nlines))))))
+   hashtable))
+
+(defun remove-overlays ()
+  ;; Scraped from alphapapa's behind
+  (let ((ovs (overlays-in (point-min) (point-max))))
+    (if (cl-loop for ov in ovs
+                 thereis (overlay-get ov :org-histogram))
+        (dolist (ov ovs)
+          (when (overlay-get ov :org-histogram)
+            (delete-overlay ov))))))
+
+
 
 (defun print-stats ()
   "Released."
   (maphash
-   (lambda (levhead linperc)
-     (let ((indent (make-string (* 4 (car levhead)) ? ))
-           (header (or (cdr levhead) "-root-"))
-           (nlines (or (plist-get linperc :nlines) 0))
-           (percnt (or (plist-get linperc :percent) 0)))
+   (lambda (head info)
+     (let ((indent (make-string (* 4 (car head)) ? ))
+           (header (or (cdr head) "{root}"))
+           (nlines (or (plist-get info :nlines) 0))
+           (percnt (or (plist-get info :percent) 100)))
        (insert
-        (format "\n;;%s[%4d,%3.0f] -- %s "
-                indent nlines percnt header))))
+        (format "\n;;%s %3.0f -- %s {%d}"
+                indent percnt header nlines))))
    (with-current-buffer "lorum.org" (process-visible))))
 
 
-
-;;[9770,  0] -- -root- 
-;;    [2613, 27] -- Galaxy 
-;;    [1928, 20] -- Projects 
-;;    [ 137,  1] -- Talks and Conferences 
-;;    [1186, 12] -- Teaching 
-;;        [ 740, 62] -- 2019 WS 
-;;        [ 429, 36] -- Students / HiWis 
-;;        [  16,  1] -- Other 
-;;    [  97,  1] -- Recreation 
-;;    [ 661,  7] -- Productivity 
-;;    [1179, 12] -- Papers 
-;;        [ 535, 45] -- Reviews 
-;;            [ 367, 69] -- BIOINF-2018-2481 
-;;            [ 126, 24] -- GigaScience Galaxy scRNA paper 
-;;            [   3,  1] -- JESTCH_2019_1343 
-;;            [  18,  3] -- [12/12] 2019 Feb - Omer 
-;;            [  20,  4] -- [8/8] 2019 June - Viktor and Omer 
-;;        [ 562, 48] -- Read 
-;;        [  81,  7] -- Write 
-;;    [ 848,  9] -- Home Projects 
-;;    [1140, 12] -- Life 
+;; 100 -- {root} {9770}
+;;      27 -- Galaxy {2613}
+;;          41 -- Tools {1072}
+;;              82 -- Single-Cell Related {876}
+;;               0 -- Linkage Related {4}
+;;              18 -- Unrelated {191}
+;;                   4 -- [2/2] GTF2Bed12 {7}
+;;                   3 -- [1/2] ggplot2 {6}
+;;                  38 -- [1/1] UMItools {72}
+;;                  14 -- [1/2] Interactive Notebooks {26}
+;;                  41 -- [6/6] GraphEmbed {79}
+;;          15 -- Trainings {392}
+;;           4 -- Datatypes {92}
+;;           1 -- Workflows {21}
+;;           1 -- Workflow Testing {28}
+;;          29 -- Workshops {745}
+;;           9 -- [6/6] User Requests {247}
+;;           1 -- Reviews {15}
+;;      20 -- Projects {1928}
+;;       1 -- Talks and Conferences {137}
+;;      12 -- Teaching {1186}
+;;       1 -- Recreation {97}
+;;       7 -- Productivity {661}
+;;      12 -- Papers {1179}
+;;       9 -- Home Projects {848}
+;;      12 -- Life {1140}
