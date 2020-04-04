1c231b8643b4a554e02d33096267cab6bafda2df
Mehmet Tekman <mtekman89@gmail.com>
2020-04-04 23:19:18 +0200

    change history to density, naming

--- org-histogram.el
+++ org-histogram.el
@@ -1,9 +1,9 @@
-;;; org-histogram.el --- Examine the density of org headings -*- lexical-binding: t; -*-
+;;; org-density.el --- Examine the density of org headings -*- lexical-binding: t; -*-
 
 ;; Copright (C) 2020 Mehmet Tekman <mtekman89@gmail.com>
 
 ;; Author: Mehmet Tekman
-;; URL: https://github.com/mtekman/org-histogram.el
+;; URL: https://github.com/mtekman/org-density.el
 ;; Keywords: outlines
 ;; Package-Requires: ((emacs "26.1") (dash "2.17.0") (org "9.1.6"))
 ;; Version: 0.1
@@ -31,11 +31,11 @@
 (require 'org-element)
 (require 'dash)
 
-(defvar org-histogram--prntalist nil)
-(defvar org-histogram--currentmode 'bar)
+(defvar org-density--prntalist nil)
+(defvar org-density--currentmode 'bar)
 
 
-(defcustom org-histogram-percentlevels
+(defcustom org-density-percentlevels
   '(((-9 .  1)  . ▏)
     (( 2 . 10)  . ▎)
     ((11 . 20)  . ▋)
@@ -49,13 +49,13 @@
     ((91 . 110) . ████▋))
   "Set the percentage lower and upper bands and  the corresponding symbol."
   :type 'alist
-  :group 'org-histogram)
+  :group 'org-density)
 
 
-(defvar org-histogram--backupformat "%1$-5s--%3$d"
+(defvar org-density--backupformat "%1$-5s--%3$d"
   "Fallback in case an invalid format is chosen by the user.")
 
-(defcustom org-histogram-formatchoices
+(defcustom org-density-formatchoices
   '((barlinepercname . "%1$-5s |%3$-5d|%2$5.1f%%|%4$s")
     (barlineperc . "%1$-5s |%3$-5d|%2$5.1f%%")
     (barlinename . "%1$s%3$-5d|%4$s")
@@ -70,64 +70,64 @@
 Some are given here as examples.  The first is the default used on startup.
 The format takes 4 positional arguments:
  1. A string representing the percentage band as set in
-    `org-histogram-percentlevels'.
+    `org-density-percentlevels'.
  2. A float showing the current percentage
  3. An integer showing the number of lines in the section.
  4. A string with the name of header."
   :type 'alist
-  :group 'org-histogram)
+  :group 'org-density)
 
-(defun org-histogram--cycleusermodes (forw)
+(defun org-density--cycleusermodes (forw)
   "Cycle a user defined list of formats in direction FORW."
-  (let ((oh-cm org-histogram--currentmode)
-        (oh-fm (mapcar 'car org-histogram-formatchoices))
+  (let ((oh-cm org-density--currentmode)
+        (oh-fm (mapcar 'car org-density-formatchoices))
         (direc (if forw 1 -1)))
     (let* ((curr-index (position oh-cm oh-fm))
            (next-index (mod (+ curr-index direc) (length oh-fm)))
            (next-umode (nth next-index oh-fm)))
-      (setq org-histogram--currentmode next-umode)
-      (org-histogram--setoverlays)
+      (setq org-density--currentmode next-umode)
+      (org-density--setoverlays)
       (message "Mode: %s" next-umode))))
 
-(defun org-histogram-cycleusermodes-forw ()
+(defun org-density-cycleusermodes-forw ()
   "Cycle user modes forwards."
   (interactive)
-  (org-histogram--cycleusermodes t))
+  (org-density--cycleusermodes t))
 
-(defun org-histogram-cycleusermodes-back ()
+(defun org-density-cycleusermodes-back ()
   "Cycle user modes backwards."
   (interactive)
-  (org-histogram--cycleusermodes nil))
+  (org-density--cycleusermodes nil))
 
-(defun org-histogram--makekey (level header)
+(defun org-density--makekey (level header)
   "Generate key for hash using LEVEL and HEADER."
   (cons level header))
 
-(defun org-histogram--makevalues (line
+(defun org-density--makevalues (line
                                   &optional perc bounds)
   "Create a value from LINE and optional PERC, BOUNDS set."
   (list :nlines line :percent perc :bounds bounds))
 
-(defun org-histogram--getlineinf (info keyf)
+(defun org-density--getlineinf (info keyf)
   "Get line number for specific INFO keyword KEYF."
   (line-number-at-pos (plist-get info keyf)))
 
-(defun org-histogram--calcperc (child par)
+(defun org-density--calcperc (child par)
   "Calculate percentage of CHILD against PAR.
 Percentages are not super accurate, but are a good gauge."
   (/ (float (* 100 child)) par))
 
-(defun org-histogram--calcnlines (info)
+(defun org-density--calcnlines (info)
   "Calculate the number of lines from INFO."
-  (- (org-histogram--getlineinf info :end)
-     (org-histogram--getlineinf info :begin)))
+  (- (org-density--getlineinf info :end)
+     (org-density--getlineinf info :begin)))
 
-(defun org-histogram--nextmatch (point1)
+(defun org-density--nextmatch (point1)
   "Get next match from current POINT1."
   (progn (org-next-visible-heading 1)
          (not (eq point1 (point)))))
 
-(defun org-histogram--makeroot (hasher)
+(defun org-density--makeroot (hasher)
   "Perform once.  Get full bounds, and put into HASHER."
   (let ((dline (- (progn (goto-char (point-max))
                          (org-backward-sentence)
@@ -135,13 +135,13 @@ Percentages are not super accurate, but are a good gauge."
                   (progn (goto-char 0)
                          (org-next-visible-heading 1)
                          (line-number-at-pos (point)))))
-        (dkey (org-histogram--makekey 0 nil)))
+        (dkey (org-density--makekey 0 nil)))
     (move-beginning-of-line 0)
-    (puthash dkey (org-histogram--makevalues dline) hasher)
+    (puthash dkey (org-density--makevalues dline) hasher)
     dkey))
 
 
-(defsubst org-histogram--searchforward (header endmark)
+(defsubst org-density--searchforward (header endmark)
   "Search forward for HEADER up to ENDMARK.
 At first use shell argument, or failing that, without."
   (let ((res (search-forward-regexp
@@ -149,7 +149,7 @@ At first use shell argument, or failing that, without."
               endmark t)))
     (or res (search-forward-regexp header endmark))))
 
-(defun org-histogram--gettitlebounds (info)
+(defun org-density--gettitlebounds (info)
   "Get title and bounds from INFO."
   (let ((head (plist-get info :title))
         (bend (plist-get info :contents-begin))
@@ -157,64 +157,64 @@ At first use shell argument, or failing that, without."
     (when head
       (save-excursion
         (goto-char bbeg) ;; important
-        (let* ((end (org-histogram--searchforward head bend))
+        (let* ((end (org-density--searchforward head bend))
                (beg (+ 2 (search-backward "* " bbeg))))
           `(,head . (,beg . ,end)))))))
 
 
-(defun org-histogram--updateparents (lvl-now previousk)
+(defun org-density--updateparents (lvl-now previousk)
   "Get or update parent into PRNT-ALIST based on PREVIOUSK.
 From current level LVL-NOW."
   (let ((prev-lvl (car previousk))
         (prev-hdr (cdr previousk))
-        (curr-parent (car org-histogram--prntalist)))
+        (curr-parent (car org-density--prntalist)))
     (cond ((not prev-lvl)
            curr-parent)
           ((> lvl-now prev-lvl)
            ;; Gone N level's deep, push the last
            ;; heading as the new parent at level
            (car (push
-                 (org-histogram--makekey prev-lvl prev-hdr)
-                 org-histogram--prntalist)))
+                 (org-density--makekey prev-lvl prev-hdr)
+                 org-density--prntalist)))
           ;;
           ((< lvl-now prev-lvl)
            ;; Returned to a level up. Pop all levels up to.
-           (while (>= (caar org-histogram--prntalist) lvl-now)
-             (pop org-histogram--prntalist))
-           (car org-histogram--prntalist))
+           (while (>= (caar org-density--prntalist) lvl-now)
+             (pop org-density--prntalist))
+           (car org-density--prntalist))
           (t curr-parent))))
 
-(defun org-histogram--processvisible ()
+(defun org-density--processvisible ()
   "The idea is to get stats only for the visible portions of the buffer.
 To investigate further, expand a heading."
   (save-excursion
-    (setq org-histogram--prntalist nil)
+    (setq org-density--prntalist nil)
     (let ((hasher (make-hash-table :test 'equal))
           (prnt-curr nil)
           (prev-key nil))
       ;; Jump to beginning and parse headers
-      (push (org-histogram--makeroot hasher) org-histogram--prntalist)
+      (push (org-density--makeroot hasher) org-density--prntalist)
       ;;
-      (while (org-histogram--nextmatch (point))
+      (while (org-density--nextmatch (point))
         (let ((info (cadr (org-element-at-point))))
           (let ((level (plist-get info :level))
-                (bound (org-histogram--gettitlebounds info)))
+                (bound (org-density--gettitlebounds info)))
             (let ((head (car bound))
                   (hrng (cdr bound)))
               (when head
                 ;; Check and update parent
-                (setq prnt-curr (org-histogram--updateparents
+                (setq prnt-curr (org-density--updateparents
                                  level prev-key))
                 ;;
-                (let ((dline (org-histogram--calcnlines info))
-                      (elkey (org-histogram--makekey level
+                (let ((dline (org-density--calcnlines info))
+                      (elkey (org-density--makekey level
                                                      head))
                       (prnt-inf (gethash prnt-curr hasher)))
-                  (let ((percent (org-histogram--calcperc
+                  (let ((percent (org-density--calcperc
                                   dline
                                   (plist-get prnt-inf :nlines))))
                     (puthash elkey
-                             (org-histogram--makevalues dline
+                             (org-density--makevalues dline
                                                         percent
                                                         hrng)
                              hasher)
@@ -222,27 +222,27 @@ To investigate further, expand a heading."
       hasher)))
 
 
-(defun org-histogram--removeoverlays ()
+(defun org-density--removeoverlays ()
   "Remove all overlays."
   (let ((ovs (overlays-in (point-min) (point-max))))
     (if (cl-loop for ov in ovs
-                 thereis (overlay-get ov :org-histogram))
+                 thereis (overlay-get ov :org-density))
         (dolist (ov ovs)
-          (when (overlay-get ov :org-histogram)
+          (when (overlay-get ov :org-density)
             (delete-overlay ov))))))
 
 
-(defun org-histogram--getformatline ()
+(defun org-density--getformatline ()
   "Get format line, if custom, then use custom format string."
-  (or (alist-get org-histogram--currentmode org-histogram-formatchoices)
+  (or (alist-get org-density--currentmode org-density-formatchoices)
       (progn (message "using backup format.")
-             org-histogram--backupformat)))
+             org-density--backupformat)))
 
-(defun org-histogram--setoverlays (&optional event)
+(defun org-density--setoverlays (&optional event)
   "Set the overlays, and use optional EVENT data."
   (ignore event)
-  (org-histogram--removeoverlays)
-  (let ((lineform (org-histogram--getformatline)))
+  (org-density--removeoverlays)
+  (let ((lineform (org-density--getformatline)))
     (maphash
      (lambda (head info)
        (let ((bounds (plist-get info :bounds))
@@ -255,16 +255,16 @@ To investigate further, expand a heading."
                    (ovner (make-overlay (car bounds) (cdr bounds)))
                    (p-bar (cdr (--first (<= (caar it) (truncate percer)
                                             (cdar it))
-                                        org-histogram-percentlevels))))
-               (overlay-put ovner :org-histogram t)
+                                        org-density-percentlevels))))
+               (overlay-put ovner :org-density t)
                (overlay-put ovner 'face oface)
                (overlay-put ovner 'display
                             (format lineform p-bar percer
                                     nlines header))))))
-     (org-histogram--processvisible))))
+     (org-density--processvisible))))
 
 
-(defun org-histogram--printstats ()
+(defun org-density--printstats ()
   "Print stats, mostly debugging."
   (maphash
    (lambda (head info)
@@ -275,32 +275,32 @@ To investigate further, expand a heading."
        (insert
         (format "\n;;%s %3.0f -- %s {%d}"
                 indent percnt header nlines))))
-   (with-current-buffer "lorum.org" (org-histogram--processvisible))))
+   (with-current-buffer "lorum.org" (org-density--processvisible))))
 
 
 
-(defvar org-histogram--modebind
+(defvar org-density--modebind
   (let ((map (make-sparse-keymap)))
     ;; Do not inherit from parent, which would
     ;; be read-only-mode.
-    (define-key map (kbd ",") 'org-histogram-cycleusermodes-back)
-    (define-key map (kbd ".") 'org-histogram-cycleusermodes-forw)
-    (define-key map (kbd "return") 'org-histogram-mode)
+    (define-key map (kbd ",") 'org-density-cycleusermodes-back)
+    (define-key map (kbd ".") 'org-density-cycleusermodes-forw)
+    (define-key map (kbd "return") 'org-density-mode)
     map)
   "Keymap for minor mode.")
 
-(define-minor-mode org-histogram-mode
-  "The mode for org-histogram."
+(define-minor-mode org-density-mode
+  "The mode for org-density."
   nil
   "ohm"
-  org-histogram--modebind
-  (if org-histogram-mode
-      (progn (add-hook 'org-cycle-hook 'org-histogram--setoverlays)
+  org-density--modebind
+  (if org-density-mode
+      (progn (add-hook 'org-cycle-hook 'org-density--setoverlays)
              (read-only-mode t)
-             (org-histogram--setoverlays))
-    (remove-hook 'org-cycle-hook 'org-histogram--setoverlays)
-    (org-histogram--removeoverlays)
+             (org-density--setoverlays))
+    (remove-hook 'org-cycle-hook 'org-density--setoverlays)
+    (org-density--removeoverlays)
     (read-only-mode -1)))
 
-(provide 'org-histogram)
-;;; org-histogram.el ends here
+(provide 'org-density)
+;;; org-density.el ends here
