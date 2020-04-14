5e7161abeb4d4ee5eebd8e57737ddde1532f3917
Mehmet Tekman <mtekman89@gmail.com>
2020-04-14 20:22:31 +0200

    removed needless package-requires

--- org-treeusage-cycle.el
+++ org-treeusage-cycle.el
@@ -5,7 +5,6 @@
 ;; Author: Mehmet Tekman
 ;; URL: https://github.com/mtekman/org-treeusage.el
 ;; Keywords: outlines
-;; Package-Requires: ((emacs "26.1") (dash "2.17.0"))
 ;; Version: 0.2
 
 ;;; License:
